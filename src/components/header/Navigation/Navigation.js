7d011d817b3bea74145e4e7aa97a753051abcc79
Huy-sudo <hathuchuy321@gmail.com>
2021-12-01 21:02:17 +0700

    navi

--- src/components/header/Navigation/Navigation.js
+++ src/components/header/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Navigation.module.css";
+import { Menu, Dropdown, Space, Avatar } from 'antd';
 import Button from "../../helpers/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { faSearch, faSortDown, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 function Navigation() {
-  function search() {} //go to search page
+  function search() { } //go to search page
 
   return (
     <div className={classes["navbar-wrapper"]}>
@@ -30,12 +32,31 @@ function Navigation() {
             <FontAwesomeIcon icon={faSearch}>search</FontAwesomeIcon>
           </Button>
         </div>
-        
       </nav>
-      <div className={classes["login-signup"]}>
+      {/* <div className={classes["login-signup"]}>
         <a href="/">Đăng nhập</a>
         <p>|</p>
         <a href="/">Đăng ký</a>
+      </div> */}
+      <div>
+        <Dropdown
+          trigger={['click']}
+          overlay={(
+            <Menu>
+              <Menu.Item key="0">
+                <Link to={'/admin'}><FontAwesomeIcon style={{ width: 20 }} icon={faUser} /> {('Quản lý')}</Link>
+              </Menu.Item>
+              <Menu.Item key="1">
+                <a href={'/auth/logout'}><FontAwesomeIcon style={{ width: 20 }} icon={faSignOutAlt} /> {('Sign Out')}</a>
+              </Menu.Item>
+            </Menu>
+          )}
+        >
+          <Space className="cursor-pointer">
+            <Avatar >USER</Avatar>
+            <FontAwesomeIcon style={{ fontSize: 20 }} icon={faSortDown} />
+          </Space>
+        </Dropdown>
       </div>
     </div>
   );
