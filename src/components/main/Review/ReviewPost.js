c660cfeb513a990b82c50924afa3ad8a60da9b7d
KNP <68104572+Nguyen070401@users.noreply.github.com>
2021-12-02 17:39:53 +0700

    Update ReviewPost.js

--- src/components/main/Review/ReviewPost.js
+++ src/components/main/Review/ReviewPost.js
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import classes from "./ReviewPost.module.css";
 import Button from "../../helpers/Button";
 
-function ReviewPost(props) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(5);
-
-  document.querySelector("body").className = classes.background;
+function Review(props) {
+    document.querySelector("body").className = classes.background;
   return (
-    <section className={classes.container}>
+    <section>
       {props.data.map((post) => {
         return (
           <div key={post.id} className={classes["content-wrapper"]}>
             <div className={classes["place-wrapper"]}>
               <h2>{post.placename}</h2>
-              <p><b>Rating: </b>{post.rating}</p>
+              <p className={classes["rating-number"]} ><b>Rating: </b>{post.rating}</p>
               <Button className={classes.more}>Xem bài</Button>
             </div>
             <div className={classes["review-wrapper"]}>
-              <h3><em>{post.postname}</em></h3>
-              <p>Tác giả: {post.author}</p>
+              <h3 className={classes["post-name"]} ><em>{post.postname}</em></h3>
+              <p className={classes["authorname"]}>Tác giả: {post.author}</p>
               <br />
-              <p>Ngày đăng: {post.date}</p>
+              <p className={classes["authordate"]}>Ngày đăng: {post.date}</p>
               <br />
-              <p>Lượt xem: {post.view}</p>
+              <p className={classes["rateview"]}>Lượt xem: {post.view}</p>
               <br />
             </div>
             <div className={classes["img-wrapper"]}>
@@ -36,4 +33,4 @@ function ReviewPost(props) {
   );
 }
 
-export default ReviewPost;
+export default Review;
